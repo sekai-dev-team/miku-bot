795afb2efa5595b2b8d39891a722fc6bd96deea8
hikari <hikari.gyx@gmail.com>
2026-01-25 15:19:48 +0800

    fix/docker: inject pysqlite3 for chromadb support

--- docker/_main.py
+++ docker/_main.py
@@ -1,3 +1,13 @@
+import sys
+
+# Pysqlite3 injection for ChromaDB
+# fix: "Unsupported vector store provider: chromadb" caused by old sqlite3 in docker
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import nonebot
 import bot  # noqa: F401
 
